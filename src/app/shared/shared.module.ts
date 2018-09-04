ba1417e174171f8343c6b2ca6a4e4aa885a05b47
Jonas Spohr <jonasspohr@gmail.com>
2018-09-04 13:31:43 -0700

    last changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,18 +10,18 @@ import { SparklineDirective } from "./directives/sparklines/sparkline.directive"
 import { DropzoneDirective } from "./directives/dropzone/dropzone.directive";
 import { InputFloatDirective } from "./directives/input-float/input-float.directive";
 
-import { TodoListsComponent } from "./components/widgets/todo-lists/todo-lists.component";
-import { PieChartsComponent } from "./components/widgets/pie-charts/pie-charts.component";
-import { RandomPostComponent } from "./components/widgets/random-post/random-post.component";
-import { RecentPostsComponent } from "./components/widgets/recent-posts/recent-posts.component";
-import { VisitorsComponent } from "./components/widgets/visitors/visitors.component";
-import { PastDaysComponent } from "./components/widgets/past-days/past-days.component";
-import { PhotosComponent } from './components/widgets/photos/photos.component';
-import { TasksComponent } from './components/widgets/tasks/tasks.component';
-import { ContactsComponent } from './components/widgets/contacts/contacts.component';
-import { RatingsComponent } from './components/widgets/ratings/ratings.component';
-import { ProfileComponent } from './components/widgets/profile/profile.component';
-import { RecentSignupsComponent } from './components/widgets/recent-signups/recent-signups.component';
+//import { TodoListsComponent } from "./components/widgets/todo-lists/todo-lists.component";
+//import { PieChartsComponent } from "./components/widgets/pie-charts/pie-charts.component";
+//import { RandomPostComponent } from "./components/widgets/random-post/random-post.component";
+//import { RecentPostsComponent } from "./components/widgets/recent-posts/recent-posts.component";
+//import { VisitorsComponent } from "./components/widgets/visitors/visitors.component";
+//import { PastDaysComponent } from "./components/widgets/past-days/past-days.component";
+//import { PhotosComponent } from './components/widgets/photos/photos.component';
+//import { TasksComponent } from './components/widgets/tasks/tasks.component';
+//import { ContactsComponent } from './components/widgets/contacts/contacts.component';
+//import { RatingsComponent } from './components/widgets/ratings/ratings.component';
+//import { ProfileComponent } from './components/widgets/profile/profile.component';
+//import { RecentSignupsComponent } from './components/widgets/recent-signups/recent-signups.component';
 
 @NgModule ({
   declarations: [
@@ -34,18 +34,18 @@ import { RecentSignupsComponent } from './components/widgets/recent-signups/rece
     InputFloatDirective,
 
     // Components
-    PastDaysComponent,
-    TodoListsComponent,
-    PieChartsComponent,
-    RandomPostComponent,
-    RecentPostsComponent,
-    VisitorsComponent,
-    PhotosComponent,
-    TasksComponent,
-    ContactsComponent,
-    RatingsComponent,
-    ProfileComponent,
-    RecentSignupsComponent
+    // PastDaysComponent,
+    // TodoListsComponent,
+    // PieChartsComponent,
+    // RandomPostComponent,
+    // RecentPostsComponent,
+    // VisitorsComponent,
+    // PhotosComponent,
+    // TasksComponent,
+    // ContactsComponent,
+    // RatingsComponent,
+    // ProfileComponent,
+    // RecentSignupsComponent
   ],
   imports: [
     CommonModule,
@@ -62,18 +62,18 @@ import { RecentSignupsComponent } from './components/widgets/recent-signups/rece
     InputFloatDirective,
 
     // Components
-    PastDaysComponent,
-    TodoListsComponent,
-    PieChartsComponent,
-    RandomPostComponent,
-    RecentPostsComponent,
-    VisitorsComponent,
-    PhotosComponent,
-    TasksComponent,
-    ContactsComponent,
-    RatingsComponent,
-    ProfileComponent,
-    RecentSignupsComponent,
+    // PastDaysComponent,
+    // TodoListsComponent,
+    // PieChartsComponent,
+    // RandomPostComponent,
+    // RecentPostsComponent,
+    // VisitorsComponent,
+    // PhotosComponent,
+    // TasksComponent,
+    // ContactsComponent,
+    // RatingsComponent,
+    // ProfileComponent,
+    // RecentSignupsComponent,
   ]
 })
 
