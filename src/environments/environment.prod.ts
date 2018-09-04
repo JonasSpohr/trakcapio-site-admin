22a0fba8592ccd94f3056e69663022c8fd4209fd
Jonas Spohr <jonasspohr@gmail.com>
2018-09-04 16:39:52 -0700

    removed unused files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  ENV: 'PROD',
+  API_ENDPOINT_DEBUG : 'http://localhost:3000/api/',
+  API_ENDPOINT_PROD : 'https://trackapio.herokuapp.com/api/',
+  API_ENDPOINT_HOMOLOG: 'https://trackapio.herokuapp.com/api/'
 };
